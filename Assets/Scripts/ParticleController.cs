20058bad9d1512d95ead4f3515abf27cb6fd1e18
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-12-01 17:10:37 +0900

    Added particle effecst.

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleController : MonoBehaviour
+{
+    float emissionRate;
+    float startSpeed;
+    ParticleSystem.Particle[] particles;
+
+    void Start ()
+    {
+        emissionRate = particleSystem.emissionRate;
+        startSpeed = particleSystem.startSpeed;
+        particles = new ParticleSystem.Particle[particleSystem.maxParticles];
+    }
+    
+    void Update ()
+    {
+        var currentSpeed = RoadMaker.instance.speed;
+        var ratio = currentSpeed / startSpeed;
+        var speed = startSpeed * ratio;
+
+        particleSystem.emissionRate = emissionRate * ratio;
+        particleSystem.startSpeed = speed;
+
+        var particleCount = particleSystem.GetParticles (particles);
+
+        for (var i = 0; i < particleCount; i++)
+        {
+            var v = particles[i].velocity;
+            v.z = -speed;
+            particles[i].velocity = v;
+        }
+
+        particleSystem.SetParticles (particles, particleCount);
+    }
+}
